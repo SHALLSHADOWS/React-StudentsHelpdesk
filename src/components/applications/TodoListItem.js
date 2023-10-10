193b7da18bbd8b5df52c479ecd82c8f2ce40232d
SHALLSHADOWS <docteurston@gmail.com>
2023-10-10 03:39:04 +0000

    Add Stuff236

--- src/components/applications/TodoListItem.js
+++ src/components/applications/TodoListItem.js
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from 'react';
 
 import {
@@ -16,58 +15,62 @@ import { NavLink } from 'react-router-dom';
 import Select from 'react-select';
 import IntlMessages from 'helpers/IntlMessages';
 import CustomSelectInput from 'components/common/CustomSelectInput';
+import { createNotification } from 'containers/ui/NotificationExamples';
+import { assignTicketRequest } from 'redux/todo/saga';
 
 import { Colxx } from '../common/CustomBootstrap';
 
-
 const TodoListItem = ({ item, handleCheckChange, isSelected }) => {
-
-
+  const [categoryName, setCategoryName] = useState('');
   const [modalBasic, setModalBasic] = useState(false);
   const [selectedOptionLO, setSelectedOptionLO] = useState('');
 
+  const [modalLong, setModalLong] = useState(false);
+
+  //
   const [selectData, setSelectData] = useState([]);
-useEffect(() => {
-  fetch('http://localhost:8080/api/supports/findAll')
-    .then((response) => response.json())
-    .then((data) => {
-      const newSelectData = data.map((items, index) => ({
-        label: items.name,
-        value: items.name,
-        key: items.id,
-      }));
-      setSelectData(newSelectData);
-    })
-    .catch((error) => console.error('Error:', error));
-}, []);
+  useEffect(() => {
+    fetch('http://localhost:8080/api/supports/findAll')
+      .then((response) => response.json())
+      .then((data) => {
+        const newSelectData = data.map((items, index) => ({
+          label: items.name,
+          value: items.name,
+          key: items.id,
+        }));
+        setSelectData(newSelectData);
+      })
+      .catch((error) => console.error('Error:', error));
+  }, []);
 
+  useEffect(() => {
+    fetch(`http://localhost:8080/api/categories/${item.category_id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setCategoryName(data.data.name);
+      })
+      .catch((error) => console.error(error));
+  }, [item.category_id]);
 
+  const assignTicket = async () => {
+    try {
+      const ticketId = item.id; // l'ID du ticket sur lequel vous avez cliqué
+      const supportId = selectedOptionLO.key; // l'ID du support sélectionné
 
-const assignTicket = async () => {
-  const ticketId = item.id; // l'ID du ticket sur lequel vous avez cliqué
-  const supportId = selectedOptionLO.key; // l'ID du support sélectionné
+      // Call the assignTicketRequest function
+      await assignTicketRequest(ticketId, supportId);
 
-  const response = await fetch('http://localhost:8080/api/TicketAssign/assign-ticket', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ ticketId, supportId }),
-  });
+      // You can add any additional logic here, such as updating UI state
+      // or displaying a success message.
+    } catch (error) {
+      createNotification('error', 'filled', 'Faild to assign');
+    }
 
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
-  }else{
-    console.log(response)
-  }
-  setModalBasic(false);
-    const updatedItemResponse = await fetch(`http://localhost:8080/api/tickets/${ticketId}`);
-    const updatedItemData = await updatedItemResponse.json();
+    createNotification('success', 'filled', 'Ticket Assign successful');
 
-  return  response.json();
-};
+    setModalBasic(false);
+  };
 
-console.log(item)
   return (
     <Colxx xxs="12">
       <Card className="card d-flex mb-3">
@@ -79,30 +82,56 @@ console.log(item)
               id={`toggler${item.id}`}
               className="list-item-heading mb-0 truncate w-40 w-xs-100  mb-1 mt-1"
             >
-              <span className="align-middle d-inline-block">{item.subject}</span>
+              <span className="align-middle d-inline-block">
+                {item.subject}
+              </span>
             </NavLink>
             {/*nom de la categories*/}
             <p className="mb-1 text-muted text-small w-15 w-xs-100">
-              {item.status}
+              {categoryName}
             </p>
 
             <p className="mb-1 text-muted text-small w-15 w-xs-100">
               {item.create_date}
             </p>
+
             <div className="w-15 w-xs-100">
               <Badge color={item.labelColor} pill>
                 {item.status}
               </Badge>
             </div>
 
-            <div>
+            <div className="w-15 w-xs-100">
+              <Button
+                color="primary"
+                outline
+                onClick={() => setModalLong(true)}
+              >
+                <IntlMessages id="View Description" />
+              </Button>
+              <Modal isOpen={modalLong} toggle={() => setModalLong(!modalLong)}>
+                <ModalHeader>Modal title</ModalHeader>
+                <ModalBody>{item.description}</ModalBody>
+                <ModalFooter>
+                  <Button color="secondary" onClick={() => setModalLong(false)}>
+                    Cancel
+                  </Button>
+                </ModalFooter>
+              </Modal>
+            </div>
+
+            <div className="w-15 w-xs-100 d-flex align-items-center">
               <Button
                 color="primary"
                 outline
                 onClick={() => setModalBasic(true)}
+                style={{
+                  display: item.status === 'UNASSIGN' ? 'block' : 'none',
+                }}
               >
                 <IntlMessages id="Assign" />
               </Button>
+
               <Modal
                 isOpen={modalBasic}
                 toggle={() => setModalBasic(!modalBasic)}
@@ -128,7 +157,7 @@ console.log(item)
                   </div>
                 </ModalBody>
                 <ModalFooter>
-                  <Button color="primary" onClick={(assignTicket)}>
+                  <Button color="primary" onClick={assignTicket}>
                     Assign
                   </Button>{' '}
                   <Button
@@ -141,6 +170,7 @@ console.log(item)
               </Modal>
             </div>
           </CardBody>
+
           <div className="custom-control custom-checkbox pl-1 align-self-center mr-4">
             <CustomInput
               className="itemCheck mb-0"
